6274cdc1d1a6297e0b4b08111cc13bed2aced335
GitHubOfAndrew <Changjyandrew@gmail.com>
2022-06-04 01:50:08 -0700

    MatrixFactorization: Added casting of hyperparameters to floats in order to avoid incompatible types in tensorflow, included methods save_model() and from_saved() in order to save and serve the trained models, included a predict() method to output a predicted interaction table, a rank() method is planned for a future commit

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -5,6 +5,7 @@
 # - THIS RUNS ONLY ON TENSORFLOW 2.5+, USE AT YOUR OWN RISK
 
 import tensorflow as tf
+import numpy as np
 import timeit as t
 
 
@@ -12,42 +13,40 @@ class MatrixFactorization:
     """Class for a Matrix Factorization Model. A hybrid (content-based and collaborative filtering) recommender model
     that takes user embeddings and item embeddings to predict unobserved interactions in a given interaction table"""
 
-    def __init__(self, num_features, shape, user_init='uniform', item_init='uniform', minval=0, maxval=None):
+    def __init__(self, num_features, shape, U=None, V=None):
 
         """
         Arguments:\n
         - num_features: a python int, number of latent features for the user and item embeddings\n
         - shape: a python tuple of the form (n,m) where (n,m) is the shape of the interaction table we are approximating\n
-        - user_init: a python string, choose the initialization scheme for our user embeddings\n
-        - item_init: a python string, choose the initialization scheme for our item embeddings\n
-        --- NOTE: By default, user and item embeddings are both uniform at the start\n
-        --- Choices of embedding initialization:\n
-        ----- uniform: a random uniform distribution initialization from minval to maxval\n
-        ----- normal: a random normal distribution initialization with a mean of 0.5*(maxval + minval) and variance of 0.25*(maxval + minval)
+        - U: a tensorflow tensor, the initialized user embeddings\n
+        - V: a tensorflow tensor, the initialized item embeddings\n
+        --- NOTE: If user and item embeddings are not supplied (i.e. U=None and V=None), then they will be initialized using random uniform values
+        --- otherwise, we can input our own choice of initializations for each embedding that we see fit
 
         Purpose:
         Initializes the following instance attributes:\n
         - user embeddings: U ---> of shape (n, num_features)\n
         - item embeddings: V ---> of shape (m, num_features)
         """
+
         n, m = shape
         user_shape = (n, num_features)
         item_shape = (m, num_features)
 
-        # initialize user and item embeddings
-        if user_init == 'uniform':
-            self.U = tf.Variable(tf.random.uniform(shape=user_shape, minval=minval, maxval=maxval), trainable=True,
-                                 dtype=tf.float32)
-        if item_init == 'uniform':
-            self.V = tf.Variable(tf.random.uniform(shape=item_shape, minval=minval, maxval=maxval), trainable=True,
-                                 dtype=tf.float32)
+        # initialize user and item embeddings if they are not specified as inputs, otherwise, set it to input
+        if U is None:
+            self.U = tf.Variable(tf.random.uniform(shape=user_shape), trainable=True, dtype=tf.float32)
+        else:
+            self.U = U
 
-        if user_init == 'normal':
-            self.U = tf.Variable(tf.random.normal(shape=user_shape, mean=(maxval + minval)/2, stddev=(maxval + minval)/4),
-                                 trainable=True, dtype=tf.float32)
-        if item_init == 'normal':
-            self.B = tf.Variable(tf.random.normal(shape=user_shape, mean=(maxval + minval)/2, stddev=(maxval + minval)/4),
-                                 trainable=True, dtype=tf.float32)
+        if V is None:
+            self.V = tf.Variable(tf.random.uniform(shape=item_shape), trainable=True, dtype=tf.float32)
+        else:
+            self.V = V
+
+        # save inputs for later use when saving model
+        self.num_features, self.shape = num_features, shape
 
     @tf.function
     def loss(self, A, U, V, lambda_1, lambda_2):
@@ -90,6 +89,9 @@ class MatrixFactorization:
         # get history of losses and training parameters
         train_history, li_loss = {}, []
 
+        # recast hyperparameters as floats so we get no errors on ints
+        lambda_1, lambda_2 = float(lambda_1), float(lambda_2)
+
         # recast hyperparameters as tf.constant objects so it can be incorporated into graph
         lambda_1, lambda_2 = tf.constant(lambda_1), tf.constant(lambda_2)
 
@@ -117,7 +119,7 @@ class MatrixFactorization:
             # compute the total mean loss and end timer
             epoch_loss = tf.reduce_mean(loss_fn).numpy()
             end = t.default_timer()
-            
+
             # add a break in the training loop for when the loss is too low, we want to combat overfitting to this extent
             if epoch_loss <= 1e-6:
                 break
@@ -146,3 +148,71 @@ class MatrixFactorization:
                     print(f'Epoch {epoch + 1}/{epochs} | Total Runtime {total_time:.4f} sec.')
 
         return train_history
+
+    def predict(self, A=None):
+        """
+        Arguments:\n
+        - A: a tensorflow tensor, the original interaction table; by default it is None
+        --- If not None: We will clip any values that go beyond the minimum or maximum entries in the original interaction table
+        --- If None: We will directly output the model's predicted interactions based on its embeddings
+
+        Purpose:\n
+        - Infer the prediction for the interaction table
+        """
+
+        predicted_interaction = tf.matmul(self.U, tf.transpose(self.V))
+
+        if A is not None:
+
+            # perform the rounding and clipping if an interaction table is supplied
+            min_val, max_val = np.min(A.numpy()), np.max(A.numpy())
+            pred_int = tf.round(predicted_interaction)
+            pred_int = tf.where(pred_int < min_val, min_val, pred_int)
+            pred_int = tf.where(pred_int > max_val, max_val, pred_int)
+        else:
+
+            # just output the direct result of the embeddings if no interaction table is supplied
+            pred_int = predicted_interaction
+
+        return pred_int
+    
+    def rank(self):
+        pass
+
+    def save_model(self):
+        """
+        Arguments:\n
+        - Nothing
+
+        Purpose:\n
+        - Save the configuration of our model needed to generate predictions\n
+        - Have the configuration ready to throw back into our model if necessary\n
+        - Make the trained model easy to serve
+        """
+
+        # dictionary compatible with model
+        model_configuration = {'num_features': self.num_features, 'shape': self.shape,
+                               'U': self.U, 'V': self.V}
+
+        return model_configuration
+
+    @classmethod
+    def from_saved(cls, config):
+
+        """
+        Arguments:\n
+        - cls: class involved in class method\n
+        - config: a python dictionary containing the trained embeddings and input parameters used for a previous training run
+
+        Purpose:\n
+        - initialize a new model (instance of this class) with the new configuration
+
+        Note:\n
+        This is a class method so be sure to call directly with the base class
+        """
+
+        # unpack the configuration dictionary and put it into our class to initialize our new model
+        return cls(**config)
+
+
+
