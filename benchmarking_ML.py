Matrix Factorization: Fixed the initializers by normalizing the weights across all entries, resulting in significant performance increases, also added the random uniform initializer; started implementation of prediction graph classes, meant to serve as an abstraction layer for predictions; added notes on loss graphs; set new optimal benchmarking configurations for ranking-based model

benchmarking_ML.py
@@ -15,6 +15,7 @@ import random
 # our libraries
 
 from matrix_factorization import MatrixFactorization
+from initializer_graphs import NormalInitializer, UniformInitializer
 from loss_graphs import WMRBLoss
 from input_utils import *
 
@@ -83,14 +84,14 @@ if run == True:
     A_train_4plus = tf.constant(train_np_4plus, dtype=tf.float32)
     A_test_4plus = tf.constant(test_np_4plus, dtype=tf.float32)
 
-    # fit wmrb with 4+ ratings
+    # fit wmrb with 4+ ratings to push those positive ratings to the top
     tf_train_4plus = convert_to_tf_sparse(train_np_4plus)
 
     # initialize model
     n_users, n_items = A_train.shape
-    n_sampled_items = n_items // 10
+    n_sampled_items = n_items // 5
     n_components = 5
-    epochs = 200
+    epochs = 100
 
     # WE WILL BE TRAINING BOTH MSE AND WMRB MODELS WITH THE SAME FEATURES AND HYPERPARAMETERS TO COMPARE PERFORMANCE
 
@@ -98,7 +99,8 @@ if run == True:
     model_ml_100k = MatrixFactorization(n_components)
 
     # WMRB model
-    model_ml_100k_wmrb = MatrixFactorization(n_components, loss_graph=WMRBLoss(), n_users=n_users, n_items=n_items,
+    model_ml_100k_wmrb = MatrixFactorization(n_components, user_weight_graph=UniformInitializer(), item_weight_graph=UniformInitializer(),
+                                             loss_graph=WMRBLoss(), n_users=n_users, n_items=n_items,
                                              n_samples=n_sampled_items, generate_sample=True)
 
     # initialize indicator features
@@ -110,7 +112,7 @@ if run == True:
     # fit model
     model_ml_100k.fit(epochs, user_features, item_features, tf_train, lr=1e-3)
 
-    model_ml_100k_wmrb.fit(epochs, user_features, item_features, tf_train_4plus, is_sample_based=True)
+    model_ml_100k_wmrb.fit(epochs, user_features, item_features, tf_train_4plus, is_sample_based=True, lr=0.1)
 
     # score model
     k = 10
@@ -141,10 +143,5 @@ if run == True:
     # precision_test_4plus = model_ml_100k.precision_at_k(A_test_4plus, k)
     print(f'Recall @ 10 on testing set (ratings >= 4) w/ MSE: {tf.reduce_mean(recall_test_4plus).numpy()}')
     print(f'Recall @ 10 on testing set (ratings >= 4) w/ WMRB: {tf.reduce_mean(recall_test_4plus_2).numpy()}')
-    # print('\n')
 
-    # recall_test_4plus_at_5 = model_ml_100k.recall_at_k(A_test_4plus, k=5)
-    # precision_test_4plus_at_5 = model_ml_100k.precision_at_k(A_test_4plus, k=5)
-    # print(f'Recall @ 5 on testing set (ratings >= 4): {tf.reduce_mean(recall_test_4plus_at_5).numpy()}')
-    # print(f'Precision @ 5 on testing set (ratings >= 4): {tf.reduce_mean(precision_test_4plus_at_5).numpy()}')
 

initializer_graphs.py
@@ -31,5 +31,22 @@ class NormalInitializer(Initializer):
         :param n_components:
         :return:
         """
-        weight = tf.Variable(tf.random.normal(shape=(n_features, n_components), dtype=tf.float32), trainable=True)
+        weight = tf.Variable(tf.math.l2_normalize(tf.random.normal(shape=(n_features, n_components), dtype=tf.float32)), trainable=True)
+        return weight
+
+
+class UniformInitializer(Initializer):
+    """
+    A subclass of Initializer, representing the object to initialize weights that are uniformly distributed
+    """
+
+    def initialize_weights(self, n_features, n_components):
+        """
+        NOTE: Look in docstring for Initializer().initialize_weights()
+
+        :param n_features:
+        :param n_components:
+        :return:
+        """
+        weight = tf.Variable(tf.math.l2_normalize(tf.random.uniform(shape=(n_features, n_components), dtype=tf.float32)), trainable=True)
         return weight

predict_graphs.py
@@ -3,8 +3,33 @@
 import tensorflow as tf
 from abc import *
 
-class PredictionGraphs(ABC):
+class PredictionGraph(ABC):
     """
     An abstract base class representing the prediction graphs. These will be objects that compute user-item relationships.
     """
-    pass
+    @abstractmethod
+    def get_prediction(self, user_embedding, item_embedding):
+        """
+        This will give a prediction score based on the user and item embeddings.
+
+        NOTE: This method should be tf-tracing compatible as it must be included in the nodes of the computational graph during gradient computation, therefore, only TensorFlow methods should be utilized in creating any custom prediction graphs.
+
+        :param user_embedding: a tensorflow tensor: the user embedding, of shape [n_users, n_components]
+        :param item_embedding: a tensorflow tensor: the item embedding, of shape [n_items, n_components]
+        :return: a tensorflow tensor: the prediction, of shape [n_users, n_items] (if otherwise, we will indicate in the respective docstring)
+        """
+        pass
+
+
+class DotProductPrediction(PredictionGraph):
+    """
+    A subclass of the PredictionGraph, abstraction for the dot product prediction for user and item embeddings.
+    """
+
+    def get_prediction(self, user_embedding, item_embedding):
+        """
+        :param user_embedding:
+        :param item_embedding:
+        :return:
+        """
+        return tf.matmul(user_embedding, tf.transpose(item_embedding))